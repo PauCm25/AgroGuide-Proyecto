4ce71be75deb1f15a7f82ace526ffee952887adf
Paula Castro <167238737+PauCm25@users.noreply.github.com>
2025-11-17 18:23:54 -0500

    Creación de los gateways encargados de las consultas a la base de datos

---
+++ guia/src/main/java/com/agroguide/guia/infraestructure/driver_adapter/jpa_repository/favoritos/FavoritosDataJpaRepository.java
@@ -0,0 +1,7 @@
+package com.agroguide.guia.infraestructure.driver_adapter.jpa_repository.favoritos;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FavoritosDataJpaRepository extends JpaRepository<FavoritosData,Long> {
+}
