fac361f6f7b94d83f2622bc9374fe13faaafdd8c
Paula Castro <167238737+PauCm25@users.noreply.github.com>
2025-11-17 18:26:14 -0500

    Primera implementación del gateway de Favoritos en infraestructura (No se han creado los métodos aún)

---
+++ guia/src/main/java/com/agroguide/guia/infraestructure/driver_adapter/jpa_repository/favoritos/FavoritosDataGatewayImpl.java
@@ -0,0 +1,11 @@
+package com.agroguide.guia.infraestructure.driver_adapter.jpa_repository.favoritos;
+
+
+import com.agroguide.guia.domain.model.gateway.FavoritosGateway;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@RequiredArgsConstructor
+@Component
+public class FavoritosDataGatewayImpl implements FavoritosGateway {
+}
