3ae87e5965fab893fecd4d1e91fa492b6045b865
Paula Castro <167238737+PauCm25@users.noreply.github.com>
2025-11-18 10:10:50 -0500

    Nuevas consultas personalizadas para el objeto favoritos

--- guia/src/main/java/com/agroguide/guia/infraestructure/driver_adapter/jpa_repository/favoritos/FavoritosDataJpaRepository.java
+++ guia/src/main/java/com/agroguide/guia/infraestructure/driver_adapter/jpa_repository/favoritos/FavoritosDataJpaRepository.java
@@ -1,7 +1,11 @@
 package com.agroguide.guia.infraestructure.driver_adapter.jpa_repository.favoritos;
 
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface FavoritosDataJpaRepository extends JpaRepository<FavoritosData,Long> {
+    Page<FavoritosData> findByUsuarioId(Long usuarioId, Pageable pageable);
+    FavoritosData findByUsuarioIdAndIdGuia(Long usuarioId, Long idGuia);
 }
