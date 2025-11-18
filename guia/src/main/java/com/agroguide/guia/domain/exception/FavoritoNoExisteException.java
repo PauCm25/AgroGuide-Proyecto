5df81d7e65220c98c63c1e99ba8ed42fdb1cbddb
Paula Castro <167238737+PauCm25@users.noreply.github.com>
2025-11-18 10:08:29 -0500

    Nueva excepción para mostrar que no existe una guía dentro de favoritos

---
+++ guia/src/main/java/com/agroguide/guia/domain/exception/FavoritoNoExisteException.java
@@ -0,0 +1,7 @@
+package com.agroguide.guia.domain.exception;
+
+public class FavoritoNoExisteException extends RuntimeException {
+    public FavoritoNoExisteException(String message) {
+        super(message);
+    }
+}
