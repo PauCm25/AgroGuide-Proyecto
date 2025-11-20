fabfab02509be16ca535d3077d687f2f34098716
daniela1415 <dandreacastro@ucundinamarca.edu.co>
2025-11-20 00:35:13 -0500

    Revert "Eliminando claves AWS del proyecto"
    This reverts commit c43417a07b0b92d782be378667654ba97be57e51.

--- guia/src/main/java/com/agroguide/guia/application/config/AWSConfig.java
+++ guia/src/main/java/com/agroguide/guia/application/config/AWSConfig.java
@@ -1,21 +1,4 @@
 package com.agroguide.guia.application.config;
 
 public class AWSConfig {
-<<<<<<< HEAD
-=======
-    //@Bean
-//    public SqsClient sqsClient (){
-//        return SqsClient.builder()
-//                .region(Region.US_EAST_2)
-//                .credentialsProvider(
-//                        StaticCredentialsProvider.create(
-//                                AwsBasicCredentials.create(
-//                                        "AKIAVS5SG6BBHTIMC65X",
-//                                        "W6vqf6D9AKCdnjk0SXyke1RTfTwEoPHv19WQCWQE"
-//                                )
-//                        )
-//                )
-//                .build();
-//    }
->>>>>>> 7196e74 (Guardando cambios antes de rebase)
 }
