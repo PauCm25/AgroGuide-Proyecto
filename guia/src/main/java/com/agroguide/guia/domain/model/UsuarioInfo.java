a6257146c3d3567d25afade1714da366cecbb0f9
Paula Castro <167238737+PauCm25@users.noreply.github.com>
2025-11-20 02:04:03 -0500

    Creación de la clase UsuarioInfo, para acoplar los datos que serán traídos desde el microservicio externo Auth

---
+++ guia/src/main/java/com/agroguide/guia/domain/model/UsuarioInfo.java
@@ -0,0 +1,17 @@
+package com.agroguide.guia.domain.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor //CREACION DEL CONSTRUCTOR
+@NoArgsConstructor
+@Setter //MODIFICA ATRIBUTO
+@Getter //OBTENER ATRIBUTO
+
+public class UsuarioInfo {
+    private Long id;
+    private String nombre;
+    private String tipoUsuario;
+}
